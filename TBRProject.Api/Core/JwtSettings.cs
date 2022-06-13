9a1c89370ececf28591238b5ff32e23efadf3bd4
AnjaZ <anjazubac1@gmail.com>
2022-06-13 13:57:30 +0200

    Delete TBRProject.Api directory

--- TBRProject.Api/Core/JwtSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TBRProject.Api.Core
-{
-    public class JwtSettings
-    {
-        public int Minutes { get; set; }
-        public string Issuer { get; set; }
-        public string SecretKey { get; set; }
-    }
-}

8215a0a39abbbfa2ad67a08f2ff96b068b010161
AnjaZ <anjazubac1@gmail.com>
2022-06-13 13:58:11 +0200

    Delete TBRProject.Application directory

--- TBRProject.Application/UseCases/Queries/IFindUserQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using TBRProject.Application.UseCases.DTO;
-
-namespace TBRProject.Application.UseCases.Queries
-{
-    public interface IFindUserQuery : IQuery<int, FindUserDto>
-    {
-    }
-}
